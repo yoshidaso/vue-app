2d599a188e7262694c20dc4454f00dddc83852e3
sota <hitoesouta@gmail.com>
2025-10-13 12:53:39 +0900

    renew template

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,35 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, ref } from 'vue'
+
+const count = ref(0)
+const increment = () => {
+  count.value++
+}
+const incrementAge = () => {
+  instructor.age++
+}
+const instructor = reactive({
+  name: 'John',
+  age: 30,
+  email: 'john@example.com',
+})
+console.log(instructor)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <button @click="increment">Count is: {{ count }}</button>
+  <h2>Instructor name is: {{ instructor.name }}</h2>
+  <h2>
+    Instructor age is: {{ instructor.age }}
+    <button @click="incrementAge">Increment</button>
+  </h2>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+button {
+  margin-top: 20px;
+  margin-left: 20px;
+  background-color: #42b983;
+  color: black;
 }
 </style>
